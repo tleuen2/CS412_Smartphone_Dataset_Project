1a908e5ca250110e4448f402d031ac58abdbd8d8
Thomas Leuenberger <tleuen2@uic.edu>
2019-11-28 12:31:17 -0600

    Completed data parser, mean vector calculation, and covariance calculation

--- MultivariateClassifier.py
+++ MultivariateClassifier.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pprint
+import pandas as pd
 
 debug = True
 
@@ -12,12 +13,14 @@ class phoneData:
 
     def __init__(self):
         self.data_dict = {}
-        self.mean_vectors = {}        
+        self.mean_vectors = {}  
+        self.covariance = {}
         
     def add_class(self, label):
         # Each label has its own array of values
         self.data_dict[label] = []
         self.mean_vectors[label] = []
+        self.covariance[label] = []
 
     def add_row(self, label, row):
         # Stack the new row into our data array
@@ -33,14 +36,36 @@ class phoneData:
             return True
             
     def calculate_mean_vector(self):
+        current_column = []
+        workspace = []
+        current_sum = 0
+        current_mean = 0
         for label in self.mean_vectors:
+            workspace = self.data_dict[label]
+            for i in range(len(workspace[0])):
+                current_column = workspace[:,i]
+                current_sum = np.sum(current_column)
+                mean = current_sum / len(workspace[0])
+                self.mean_vectors[label].append(mean)
+
+    def calculate_covariances(self):
+        workspace = []
+        current_cov = []
+        for label in self.covariance:
+            workspace = self.data_dict[label]
+            workspace = np.transpose(workspace)
+            current_cov = np.cov(workspace)
+            self.covariance[label] = current_cov
             
-            
+        
     def show_info(self):
         print('---------Info about data containter---------')
         print('*Number of Classes = ', len(self.data_dict.keys()))
         for j in self.data_dict:
-            print('*The shape of label ' , j , ' is ' , self.data_dict[j].shape)
+            print('*Info for label ', j)
+            print('**Shape of data :',  self.data_dict[j].shape)
+            print('**Length of mean vector :', len(self.mean_vectors[j]), ' entries')
+            print('**Shape of covariance matrix :', self.covariance[j].shape)
         print('---------Info about data containter---------')
     
 def seperate_by_class(labels, data):
@@ -53,14 +78,11 @@ def seperate_by_class(labels, data):
             my_data.add_class(labels[i])
         my_data.add_row(labels[i], data[i])        
     return my_data
-        
-
-def get_mean_vector(my_data):
-    ret_val = {}
-    for key in 
 
 def main():
     train_data = seperate_by_class(training_labels, training_data)
+    train_data.calculate_mean_vector()
+    train_data.calculate_covariances()
     train_data.show_info()
     
 
